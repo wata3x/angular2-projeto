353675986ba9b45d6e09d61ea8078b6b1d4eb75c
wata3x <felliperocha98@gmail.com>
2016-10-27 00:29:52 -0200

    começando com angular 2 e git

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My Angular App</h1>'
+  template: '<h1>My3 Angular App</h1>'
 })
 export class AppComponent { }
